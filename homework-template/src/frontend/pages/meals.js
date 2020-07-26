ba94c18711ab2d80ac67046596b2271c7bbeb2d1
Rufaida <eng.rufaidajumaa@gmail.com>
2020-07-26 21:37:17 +0200

    Shorting the code

--- homework-template/src/frontend/pages/meals.js
+++ homework-template/src/frontend/pages/meals.js
@@ -1,86 +1,40 @@
-window.handleMealsRequest = (params,query) => {
+window.handleMealsRequest = async (params,query) => {
   function isEmpty(obj) {
     for (let key in obj) {
       return false;
     }
     return true;
   }
+
+  // If the query has no parameters (/meals)
   if( isEmpty(query)){
     document.querySelector('.content').insertAdjacentHTML('beforeend', `
     <h2>ALL Meals</h2>
     <div class="container">
-      <div class="row justify-content-center text-center">
-        <div class="  col-md-6  col-lg-4">
-          <div class="card" style="width: 18rem;">
-            <img src="../pictures/food1.jpg" class="card-img-top" alt="../pictures/food2.jpg">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary card-btn">Book</a>
-            </div>
-          </div>
-        </div>
-        <div class="  col-md-6    col-lg-4">
-          <div class="card" style="width: 18rem;">
-            <img src="../pictures/food2.jpg" class="card-img-top" alt="../pictures/food2.jpg">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary card-btn">Book</a>
-            </div>
-          </div>
-        </div>
-        <div class="  col-md-6   col-lg-4">
-          <div class="card" style="width: 18rem;">
-            <img src="../pictures/food1.jpg" class="card-img-top" alt="../pictures/food2.jpg">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary card-btn">Book</a>
-            </div>
-          </div>
-        </div>
-        <div class="  col-md-6  col-lg-4">
-          <div class="card" style="width: 18rem;">
-            <img src="../pictures/food2.jpg" class="card-img-top" alt="../pictures/food2.jpg">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary card-btn">Book</a>
-              </div>
-          </div>
-        </div>
-        <div class="  col-md-6  col-lg-4">
-          <div class="card" style="width: 18rem;">
-            <img src="../pictures/food2.jpg" class="card-img-top" alt="../pictures/food2.jpg">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary card-btn">Book</a>
-            </div>
-          </div>
-        </div>
-        <div class=" col-md-6  col-lg-4">
-          <div class="card" style="width: 18rem;">
-            <img src="../pictures/food1.jpg" class="card-img-top" alt="../pictures/food2.jpg">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary card-btn">Book</a>
-            </div>
-          </div>
-        </div>
+      <div class="row justify-content-center text-center all-meals">
+        
       </div>
     </div>
-    <ul class = 'all-meals'></ul>`);
+    `);
        
-  const mealsResponse = fetch("/api/meals")
+  const mealsResponse = await fetch("/api/meals")
     .then((response) => response.json())
     .then((meals) => {
       meals.forEach( meal => 
       {
-        document.querySelector('.all-meals').insertAdjacentHTML('beforeend',`<li>${meal.title}</li>`);
+        document.querySelector('.all-meals').insertAdjacentHTML('beforeend',` 
+          <div class="col-md-6  col-lg-4">
+            <div class="meals-card" style="width: 18rem;">
+              <img src="../pictures/food1.jpg" class="meals-card-img-top card-img-top" alt="../pictures/food2.jpg">
+              <div class="meals-card-body">
+                <h5 class="meals-card-title">${meal.title}</h5>
+                <p class="meals-card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="meals/${meal.id}" class="btn btn-primary meals-card-btn">Book</a>
+              </div>
+            </div>
+          </div>`);
       });
     });
   }
 };
+
