2be9776af120d14607a789c8cbafe40f37d9c41b
Rufaida <eng.rufaidajumaa@gmail.com>
2020-07-27 12:04:25 +0200

    Add reviews/meal_id router

--- homework-template/src/frontend/main.js
+++ homework-template/src/frontend/main.js
@@ -6,3 +6,4 @@ router.on(window.handleHomeRequest).resolve();
 router.on("/meals/:id", window.handleMealRequest).resolve();
 router.on("/meals", window.handleMealsRequest).resolve();
 router.on("/reservation/:meal_id", window.handleReservationRequest).resolve();
+router.on("/reviews/:meal_id", window.handleReservationRequest).resolve();
