a4320aa813e4dc6e3bade87b52bcc662f0fe404d
Rufaida <eng.rufaidajumaa@gmail.com>
2020-07-27 20:50:58 +0200

    Add add-meal router

--- homework-template/src/frontend/main.js
+++ homework-template/src/frontend/main.js
@@ -8,3 +8,4 @@ router.on("/meals", window.handleMealsRequest).resolve();
 router.on("/reservation/:meal_id", window.handleReservationRequest).resolve();
 router.on("/reviews/:meal_id", window.handleReviewsRequest).resolve();
 router.on("/review/:meal_id", window.handleReviewRequest).resolve();
+router.on("/add-meal", window.handleAddMealRequest).resolve();
