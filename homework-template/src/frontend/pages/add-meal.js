3d2ab1501e9c25df4996ca90d3896b6e079d60c3
Rufaida <eng.rufaidajumaa@gmail.com>
2020-07-31 13:33:02 +0200

    Change the place to show all meals btn

--- homework-template/src/frontend/pages/add-meal.js
+++ homework-template/src/frontend/pages/add-meal.js
@@ -3,34 +3,32 @@ window.handleAddMealRequest = async (params) => {
      <h2>Add a new meal</h2>
      <form class="add-meal-form">
      <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-6">
-          <input type="text" class="form-control input-meal-title" placeholder="Meal title" required>
+        <div class="row justify-content-center">
+          <div class="col-6">
+            <input type="text" class="form-control input-meal-title" placeholder="Meal title" required>
+          </div>
         </div>
-      </div>
 
-      <div class="row justify-content-center">
-        <div class="col-6">
-          <textarea class="input-meal-description" rows="2" cols="" placeholder="Insert  description to the meal..." required></textarea>
+        <div class="row justify-content-center">
+          <div class="col-6">
+            <textarea class="input-meal-description" rows="2" cols="" placeholder="Insert  description to the meal..." required></textarea>
+          </div>
         </div>
-      </div>
 
-      <div class="row justify-content-center">
-        <div class="col-3">
-            <input type="number" class="form-control input-meal-maxNumberOfGuests" placeholder="Max number of guests" required>
+        <div class="row justify-content-center">
+          <div class="col-3">
+              <input type="number" class="form-control input-meal-maxNumberOfGuests" placeholder="Max number of guests" required>
+          </div>
+          <div class="col-3">
+            <input type="number" class="form-control input-meal-price" placeholder="Price" required>
+          </div>
         </div>
-        <div class="col-3">
-          <input type="number" class="form-control input-meal-price" placeholder="Price" required>
+        <div class="row justify-content-center">
+          <div class="col-6 text-center">
+            <button type="submit" class="btn btn-primary add-meal-card-btn">Submit</button>
+          </div>
         </div>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-6 text-center">
-          <button type="submit" class="btn btn-primary add-meal-card-btn">Submit</button>
-        </div>
-      </div>
-      </div>
-
-      
+     </div>
      </form>
      
      `);
@@ -65,21 +63,19 @@ window.handleAddMealRequest = async (params) => {
             document.querySelector('.content').insertAdjacentHTML('beforeend',`
             <div class="row">
               <div class="col text-center">
-                    <p class="add-meal-submitted">You have added the meal successfully</p>
+                <p class="add-meal-submitted">You have added the meal successfully</p>
               </div>
            </div>
 
            <div class="row">
-           <div class="col text-center">
-           <button class="home-all-meals-btn"><a href="/meals">Show all meals</a></button>
-       </div>
+              <div class="col text-center">
+                <button class="home-all-meals-btn"><a href="/meals">Show all meals</a></button>
+              </div>
            </div>
            `);
            console.log('Success:', data);
           }).catch((error) => {
            console.error('Error: ', error);
           });
-    };
-
-    
-  };
+    };  
+};
